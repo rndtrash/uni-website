f408408a800a4be70f6ec99e310073b131541d43
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-05-30 12:21:33 +0300

    jQuery меню (вариант 10), PHP

---
+++ php/www/php.localhost/goods.php
@@ -0,0 +1,38 @@
+<?php
+
+$art = array(
+    '1' => array(
+        'name' => 'Товар №1',
+        'weight' => '100 килограмм',
+        'cost' => '100 рублей',
+        'img' => 'https://bigrat.monster/media/bigrat.png'
+    ),
+    '2' => array(
+        'name' => 'Товар №2',
+        'weight' => '100 грамм',
+        'cost' => '100 000 рублей',
+        'img' => 'https://soggy.cat/img/soggycat.jpg'
+    )
+);
+
+header('Content-Type: application/json');
+
+if (empty($_POST['art'])) {
+    echo json_encode(
+        array(
+            'error' => 'Не указан артикуль'
+        )
+    );
+    die;
+}
+
+if (!array_key_exists($_POST['art'], $art)) {
+    echo json_encode(
+        array(
+            'error' => "Неверно указан артикуль: " . $_POST['art']
+        )
+    );
+    die;
+}
+
+echo json_encode($art[$_POST['art']]);
